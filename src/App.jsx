583072ba555a0af2988f2d83bc92a2e20da8c1f5
Prabhudutta Panigrahi <58845544+p-panigrahi@users.noreply.github.com>
2024-03-31 10:49:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './Component/Navigation';
+import './App.css';
+import HeroSection from './Component/HeroSection';
+const App = () => {
+  return (
+    <>
+        <div>
+            <Navigation/>
+            <HeroSection/>
+        </div>
+    </>
+  )
+}
+
+export default App
